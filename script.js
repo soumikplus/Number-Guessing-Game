8168a820e5988355fabebc894cdc9e26f608d67e
soumikplus <csoumik10@gmail.com>
2025-05-01 07:49:01 +0530

    second  commit

--- script.js
+++ script.js
@@ -2,9 +2,14 @@ let a = Math.floor(Math.random()*5+1)
 let guess = 1
 
 document.getElementById("submit-guess").onclick = function(){
-    let x = document.getElementById("guessField").value;
-    if(x==a){
+    let x = parseInt(document.getElementById("guessField").value);
+    if(x<1 || x>5){
+        alert("Oops! Please enter valid number between 1 and 5")
+        return;
+    }
+    if(x === a){
         alert("🎉 CONGRATULATIONS! Number of attempts: " +guess)
+        guess = 1;
         a = Math.floor(Math.random() * 5 + 1);
     }
     else if(x>a){
